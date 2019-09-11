1bc1a2d6145ad091342b18950b3565c610606d7c
unknown <ybalimova@gmail.com>
2019-09-11 14:31:21 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,18 @@
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let average = (algebra + geography + physics) / 3;
+
+console.log(average);
+
+let name = "Юлия";
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
